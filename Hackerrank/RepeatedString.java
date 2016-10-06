4f14147945c9e0d00628d2f6eed8085b205cd718
Sweta Shah <swetashah10@gmail.com>
2016-10-06 16:52:38 -0700

    Create RepeatedString.java

---
+++ Hackerrank/RepeatedString.java
@@ -0,0 +1,29 @@
+ public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String s = in.next();
+        long n = in.nextLong();
+        long numOfAsInOriginal = 0;
+        long numOfAsInString = 0;
+        long numOfRemainingElems = 0;
+        Map<Long, Long> mapOfIndexAndNumOfAs = new HashMap<Long, Long>();
+        int lenOfStr = s.length();
+        
+        if(lenOfStr >= 1 && lenOfStr <= 100){
+            
+            for(int i=0; i<lenOfStr; i++){
+                if(s.charAt(i) == 'a'){
+                   numOfAsInOriginal++; 
+                }
+                mapOfIndexAndNumOfAs.put(new Long(i), numOfAsInOriginal);
+            }
+            if(lenOfStr < n){
+                
+                numOfAsInString = (n / (lenOfStr)) * numOfAsInOriginal;
+                numOfRemainingElems = n % (lenOfStr);
+                if(numOfRemainingElems > 0)
+                  numOfAsInString += mapOfIndexAndNumOfAs.get(numOfRemainingElems-1);
+            }
+        }
+        
+        System.out.println(numOfAsInString);
+    }
