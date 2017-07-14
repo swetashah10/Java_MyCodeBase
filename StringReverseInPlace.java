767f836b663c2d037be8e6974b87306407d6fd98
Sweta Shah <swetashah10@gmail.com>
2017-07-14 01:34:12 -0700

    Create StringReverseInPlace.java

---
+++ StringReverseInPlace.java
@@ -0,0 +1,24 @@
+import java.util.Stack;
+
+public class StringReverseInPlace {
+
+	public static void main(String[] args){
+		Problem1dot2 problem1Dot2Obj = new Problem1dot2();
+		
+		String str = new String("test string"+(null));
+		System.out.println("Actual String: "+str+"   Reverse: " +problem1Dot2Obj.reverseString(str));
+	}
+
+	private String reverseString(String str) {
+		String reverseStr = new String();
+		Stack<Character> stackOfChars = new Stack<Character>();
+		for(int i=0; i<str.length(); i++){
+			stackOfChars.push(str.charAt(i));
+		}
+		
+		for(int i=0; i<str.length(); i++){
+			reverseStr+= stackOfChars.pop();
+		}
+		return reverseStr;
+	}
+}
